2eac653818a3c3c50412fd5be6a7f9bc13b65ac8
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2016-08-10 13:27:30 +0300

    released 1.3-edw1, new version 1.3-edw2

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,4 +1,7 @@
-1.3-edw1 (unreleased)
+1.3-edw2 (unreleased)
+-------------------
+
+1.3-edw1 (2016-08-10)
 -------------------
 * decreased the connection timeout to 5 seconds [dumitval]
 * extended error handling on mx connection [dumitval]
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='validate_email',
-      version='1.3-edw1',
+      version='1.3-edw2',
       download_url='git@github.com:eaudeweb/validate_email.git',
       py_modules=('validate_email',),
       author='Valentin Dumitru (fork from Syrus Akbary)',
